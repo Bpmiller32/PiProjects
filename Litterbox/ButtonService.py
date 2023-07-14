eeee4b98084f39650434fa337749b8c1d6743ed2
Bpmiller32 <89849055+Bpmiller32@users.noreply.github.com>
2023-07-14 14:25:21 -0700

    Update ButtonService.py

--- Litterbox/ButtonService.py
+++ Litterbox/ButtonService.py
@@ -1,36 +1,47 @@
 import RPi.GPIO as GPIO
 import time
 import datetime;
+from systemd import journal
 
-print("Current time: ", datetime.datetime.now())
+timer = 1800
+
+spin_output = 2
+cancel_output = 3
+
+spin_button = 20
+cancel_button = 21
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(2, GPIO.OUT)
-GPIO.setup(3, GPIO.OUT)
+GPIO.setup(spin_output, GPIO.OUT)
+GPIO.setup(cancel_output, GPIO.OUT)
+GPIO.setup(spin_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(cancel_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
-    GPIO.output(2, GPIO.LOW)
-    GPIO.output(3, GPIO.LOW)
-    print("Buttons unpressed")
-
-    time.sleep(3)
+    GPIO.output(spin_output, GPIO.LOW)
+    GPIO.output(cancel_output, GPIO.LOW)
 
     while True:
-        GPIO.output(3, GPIO.HIGH)
-        print("'Cancel' button pressed once")
-        time.sleep(0.75)
-        GPIO.output(3, GPIO.LOW)
-
-        time.sleep(3)
-
-        GPIO.output(3, GPIO.HIGH)
-        print("'Cancel' button pressed twice")
-        time.sleep(0.75)
-        GPIO.output(3, GPIO.LOW)
-
-        time.sleep(43200)
-
+        if GPIO.input(spin_button) == GPIO.LOW:
+            journal.write("Spin button pressed. Outputting signal.")
+            GPIO.output(spin_output, GPIO.HIGH)
+            time.sleep(0.75)
+            GPIO.output(spin_output, GPIO.LOW)
+        elif GPIO.input(cancel_button) == GPIO.LOW:
+            journal.write("Cancel button pressed. Outputting signal.")
+            GPIO.output(cancel_output, GPIO.HIGH)
+            time.sleep(0.75)
+            GPIO.output(cancel_output, GPIO.LOW)
+        else:
+            if timer == 0:
+                journal.write("Cancel time reached. Outputting signal, resetting timer.")
+                GPIO.output(cancel_output, GPIO.HIGH)
+                time.sleep(0.75)
+                GPIO.output(cancel_output, GPIO.LOW)
+                timer = 1800
+            else:
+                timer = timer - 1
+                time.sleep(1)
 finally:
     GPIO.cleanup()
-
